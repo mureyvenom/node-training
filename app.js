2c1fac40fbee232dfbb030decbe54852fa88e9a2
mureyvenom <holuwamurewa@gmail.com>
2021-08-03 23:52:37 +0100

    Updated projected structure and added more files/routes/controllers

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+
 const errorController = require('./controllers/error');
-// const handlebars = require('express-handlebars');
 
 const app = express();
 
-// handlebars config
-// app.engine('hbs', handlebars({
-//     extname: 'hbs',
-//     defaultLayout: '',
-//     layoutsDir: 'views/layouts/',
-//     defaultLayout: 'main-layout'
-// }));
-app.set('view engine', 'ejs');//set template engine
-app.set('views', 'views')//set views directory
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public')))
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3001);
+app.listen(3000);
